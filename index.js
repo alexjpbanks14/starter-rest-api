4fe7ddc2fa3b91b0e71bc4da718874a54b17276f
alexjpbanks14 <alexjpbanks14@gmail.com>
2023-06-30 16:57:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ var allowedOrigins = ['http://localhost:8081',
                       'http://yourapp.com'];
 app.use(cors({
   origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
     return callback(null, true);
   }
 }));
